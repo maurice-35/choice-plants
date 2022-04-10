5869ac2386beba5908847d294aa4b4af419c61b6
mauricekollewe <kollewem.35@gmail.com>
2022-04-10 20:28:26 +0100

    get request

--- plants/views.py
+++ plants/views.py
@@ -9,5 +9,22 @@ class PlantListView(APIView):
 
 	def get(self, _request):
 		plants = Plant.objects.all() # get everything from the plant in the db
+		print('PLANT 🏮', plants)
 		serialized_plants = PlantSerializer(plants, many=True) # Transforms data into python by running through serializer
+		print('SERIALIZED', serialized_plants.data)
 		return Response(serialized_plants.data, status=status.HTTP_200_OK) # return data and status code
+
+class PlantDetailView(APIView):
+
+	def get_plant(self, pk):  # getting an item from th db
+		try:
+			return Plant.objects.get(pk=pk)
+		except Plant.DoesNotExist:
+			raise NotFound(detail="🏮 can't find that plant")
+
+	def get(self, _request, pk):
+		plant = self.get_plant(pk=pk)
+		serialized_plant = PlantSerializer(show)
+		return Response(serialized_plant.data, status=status.HTTP_200_OK)
+
+
