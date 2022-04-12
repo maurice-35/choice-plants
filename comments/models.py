ca313962526f278eb779bc18c23786a06ad5efef
mauricekollewe <kollewem.35@gmail.com>
2022-04-12 23:40:31 +0100

    create comments app

---
+++ comments/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Comment(models.Model):
+		text = models.TextField(max_length=300)
+		created_at = models.DateTimeField(auto_now_add=True)
+		plant = models.ForeignKey(
+			"plants.Plant",  # this define the table where the ralationship is going to be with
+			related_name = "comments", #This is wher the comment will be called in a plant lookup
+			on_delete = models.CASCADE
+
+		)
