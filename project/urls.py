9eebea8f4ff6afa63ad458eb5a27e180c1ecf38e
mauricekollewe <kollewem.35@gmail.com>
2022-04-15 23:48:05 +0100

    create views.py & urls.py ofr comments, post a plant

--- project/urls.py
+++ project/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('plants/', include('plants.urls'))
+    path('plants/', include('plants.urls')),
+    path('comments/', include('comments.urls'))
 ]
