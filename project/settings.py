fd7c34bda23b9f1ac24004eafd9644bb17c54b6e
mauricekollewe <kollewem.35@gmail.com>
2022-04-20 22:33:00 +0100

    create jwt_auth & models.py

--- project/settings.py
+++ project/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'plants',
     'comments',
-    'category'
+    'category',
+    'jwt_auth'
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'jwt_auth.User'  # Here django other custom users
