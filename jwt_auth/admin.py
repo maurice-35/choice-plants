ed76b688197828c73029eb8105bd09a191c03ad7
mauricekollewe <kollewem.35@gmail.com>
2022-04-22 23:00:04 +0100

    register jwt_auth

--- jwt_auth/admin.py
+++ jwt_auth/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model # This gets our custom model
 
-# Register your models here.
+User = get_user_model()  # get current user model
+
+admin.site.register(User)
