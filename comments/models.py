cf4556e542106e9212afc7ee41ae9a29e749b377
mauricekollewe <kollewem.35@gmail.com>
2022-04-24 23:56:38 +0100

    create owner & authentication permission

--- comments/models.py
+++ comments/models.py
@@ -8,5 +8,12 @@ class Comment(models.Model):
 			"plants.Plant",  # this define the table where the ralationship is going to be with
 			related_name = "comments", #This is wher the comment will be called in a plant lookup
 			on_delete = models.CASCADE
-
 		)
+		owner = models.ForeignKey(
+			"jwt_auth.User", 
+			related_name="comment",
+			on_delete = models.CASCADE
+		)
+
+		def __self__(self):
+				return f"comments: {self.text}"
--- comments/views.py
+++ comments/views.py
@@ -2,14 +2,17 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.exceptions import NotFound
+from rest_framework.permissions import IsAuthenticated
 
 from comments.models import Comment
 
 from .serializers.common import CommentSerializer
 
 class CommentListView(APIView):
+			permission_classes = {IsAuthenticated, }
 
 			def post(self, request):
+					request.data["owner"] = request.user.id
 					comment_to_create = CommentSerializer(data=request.data)
 					if comment_to_create.is_valid():
 							comment_to_create.save()
