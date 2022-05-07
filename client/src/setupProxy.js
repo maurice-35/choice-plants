a7ec19dacc70540a13de43e9868c045e638690aa
mauricekollewe <kollewem.35@gmail.com>
2022-05-07 23:49:03 +0100

    add frontend

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,9 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
   app.use(
     createProxyMiddleware('/api', 
-		{ target: 'http://localhost:8000',
-    changeOrigin: true,
-  }
+		{ target: 'http://localhost:8000' }
 		)
   )
 }
