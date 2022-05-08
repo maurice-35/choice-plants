cafaef1d6eba602a7148f6f3ba93a26048a05e27
mauricekollewe <kollewem.35@gmail.com>
2022-05-08 23:58:32 +0100

    update app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import axios from 'axios'
-import './App.css';
+import { Carousel } from 'react-bootstrap'
+// import './App.css';
 
 const App = () => {
   useEffect(() => {
@@ -15,9 +16,45 @@ const App = () => {
   }, [])
 
   return (
-    <div>
-      <h1>Hello world!</h1>
-    </div>
+    <section>
+      <div>
+        <Carousel>
+          <Carousel.Item interval={1000}>
+            <img
+              className="d-block w-100"
+              src="holder.js/800x400?text=First slide&bg=373940"
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nice flower.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item interval={500}>
+            <img
+              className="d-block w-100"
+              src="holder.js/800x400?text=Second slide&bg=282c34"
+              alt="Second slide"
+            />
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Nice flower.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="holder.js/800x400?text=Third slide&bg=20232a"
+              alt="Third slide"
+            />
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>Nice flower.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </div>
+    </section>
   )
 }
 
