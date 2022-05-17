b8888ef895cbebda2f0c06270a5543230f6dde4a
mauricekollewe <kollewem.35@gmail.com>
2022-05-17 23:20:50 +0100

    update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,33 @@
-import React, { useEffect } from 'react'
-import axios from 'axios'
-import { Card, CardGroup, Row } from 'react-bootstrap'
-
+import React, { useEffect } from "react";
+import axios from "axios";
+import { Card, CardGroup, Row } from "react-bootstrap";
 
 const App = () => {
-
   useEffect(() => {
-		const getData = async () => {
-			try {
-				const { data } = await axios.get('/api/plants/')
-				console.log('DATA', data)	
-			} catch (err) {
-				console.log(err)
-			}
-		}
-		getData()
-	}, [])
+    const getData = async () => {
+      try {
+        const { data } = await axios.get("/api/plants/");
+        console.log("DATA", data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getData();
+  }, []);
 
-	return (
-		<section className="section">
-			<div className="container">
-				<Row className="grid-container">
-					<CardGroup>
-						<Card id="plant">
-							<h1>Hello World</h1>
-						</Card>
-					</CardGroup>
-				</Row>
-			</div>
-		</section>
-	)
-}
+  return (
+    <section className="section">
+      <div className="container">
+        <Row className="grid-container">
+          <CardGroup>
+            <Card id="plant">
+              <h1>Hello World</h1>
+            </Card>
+          </CardGroup>
+        </Row>
+      </div>
+    </section>
+  );
+};
 
-export default App
+export default App;
