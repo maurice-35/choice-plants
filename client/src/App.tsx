2d9777c9df08b231fb25f853d46beb2f40788816
mauricekollewe <kollewem.35@gmail.com>
2022-05-19 16:20:43 +0100

    update app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,31 +3,31 @@ import axios from "axios";
 import { Card, CardGroup, Row } from "react-bootstrap";
 
 const App = () => {
-  useEffect(() => {
-    const getData = async () => { 
-				try {
-				const	{ data } = await axios.get('/api/plants/')
-					console.log(data)
-				} catch(err) {
-					console.log(err)
-				}
+	useEffect(() => {
+		const getData = async () => {
+			try {
+				const { data } = await axios.get('/api/plants/')
+				console.log(data)
+			} catch (err) {
+				console.log(err)
 			}
-    getData();
-  }, []);
+		}
+		getData();
+	}, []);
 
-  return (
-    <section className="section">
-      <div className="container">
-        <Row className="grid-container">
-          <CardGroup>
-            <Card id="plant">
-              <h1>Hello World</h1>
-            </Card>
-          </CardGroup>
-        </Row>
-      </div>
-    </section>
-  );
+	return (
+		<section className="section">
+			<div className="container">
+				<Row className="grid-container">
+					<CardGroup>
+						<Card id="plant">
+							<h1>Hello World</h1>
+						</Card>
+					</CardGroup>
+				</Row>
+			</div>
+		</section>
+	);
 };
 
 export default App;
